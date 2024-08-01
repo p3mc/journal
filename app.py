9c490e5b87a61fea3842c28d09b74dc6c6443023
karina <karinaszczepanek@gmail.com>
2024-08-01 11:16:13 +0200

    + commented 2nd table

--- app.py
+++ app.py
@@ -24,6 +24,31 @@ def join():
 		return render_template("index.html")
 	else:
 		return render_template('join.html')
+
+# Test
+
+""" def index():
+        return render_template('index.html')
+connect = sqlite3.connect('database.db')
+connect.execute('CREATE TABLE IF NOT EXISTS FOOD (breakfast TEXT, lunch TEXT, dinner TEXT, snacks TEXT)')
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+        if request.method == 'POST':
+                breakfast = request.form['breakfast']
+                lunch = request.form['lunch']
+                dinner = request.form['dinnner']
+                snacks = request.form['snacks']
+
+                with sqlite3.connect("database.db") as users:
+                        cursor = users.cursor()
+                        cursor.execute('INSERT INTO FOOD(breakfast,lunch,dinner,snacks) VALUES (?,?,?,?,?)', (breakfast,lunch,dinner,snacks))
+                        users.commit()
+                return render_template("index.html")
+        else:
+                return render_template('join.html') """
+
+# End test
+
 @app.route('/participants')
 def participants():
 	connect = sqlite3.connect('database.db')
@@ -33,3 +58,18 @@ def participants():
 	return render_template("participants.html", data=data)
 if __name__ == '__main__':
 	app.run(debug=False)
+
+# Test
+
+""" def meals():
+        connect = sqlite3.connect('database.db')
+        cursor = connect.cursor()
+        cursor.execute('SELECT * FROM FOOD')
+        meal = cursor.fetchall()
+        return render_template("participants.html", meal=meal)
+if __name__ == '__main__':
+        app.run(debug=False) """
+
+
+
+# Ends test 
